237bdd702fe574df038b1c64e376b56056848962
ayushmavl <149374395+ayushmavl@users.noreply.github.com>
2024-07-14 12:29:53 +0530

    Add files via upload

---
+++ LINEAR.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int linearSearch(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            return i; // Element found, return its index
+        }
+    }
+    return -1; // Element not found
+}
+
+int main()
+{
+    int arr[] = {10, 20, 80, 30, 60, 50, 110, 100, 130, 170};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 100;
+
+    int result = linearSearch(arr, n, x);
+
+    if (result == -1)
+    {
+        cout << "Element is not present in array" << endl;
+    }
+    else
+    {
+        cout << "Element is present at index " << result << endl;
+    }
+
+    return 0;
+}
